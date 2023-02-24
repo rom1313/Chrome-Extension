f5aab53249221bfba5e59a26b9cab825e07cb891
rom1313 <romain.gavin@outlook.fr>
2023-02-24 07:49:16 +0100

    Added my project

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
+const genius = $
+
 let tab = ["Message 1", "Message 2", "Message 3"]
+
 document.querySelector("#button").onclick = () => {
-    console.log('yo');
-    document.querySelector("#contenu").innerHTML = tab.map(item => 
+
+    document.querySelector("#contenu").innerHTML = tab.map(item =>
         `<p>${item}<p/>`).join('')
-    
-}
-document.querySelector("#inputchat").onkeyup = (e)=>{
-console.log(e.target.value);
-document.querySelector("#contenuchat").innerHTML = `<p>${e.target.value}<p/>`
 
 }
+document.querySelector("#inputchat").oninput = (e) => {
+    console.log(this.value);
+    document.querySelector("#contenuchat").innerHTML = `<p>${e.target.value}<p/>`
+
+}
+
+genius.event("#color", "click", (e) => {
+
+    if (!window.EyeDropper) {
+        document.querySelector("#couleur").textContent =
+            "Your browser does not support the EyeDropper API";
+        return;
+    }
+    const eyeDropper = new EyeDropper();
+    eyeDropper
+        .open()
+        .then((result) => {
+            let couleur = result.sRGBHex;
+            document.body.style.backgroundColor = couleur
+            document.querySelector("#couleur").textContent = ` ${couleur}`
+            navigator.clipboard.writeText(couleur).then(function () {
+                console.log('Async: Copying to clipboard was successful!');
+            }, function (err) {
+                console.error('Async: Could not copy text: ', err);
+            });
+
+
+        })
+        .catch((e) => {
+
+        })
+
+
+
+})
+
+genius.event("#file", "mousedown", (e) => {
+    window.showOpenFilePicker()
+        .then((result => {
+            console.log(result);
+
+        }));
+
+
+})
+
+if ('geolocation' in navigator) {
+    console.log('geo ok');
+    navigator.geolocation.watchPosition((position) => {
+        console.log(position);
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+      open(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`)
+
+      });
+    /* geolocation is available */
+  } else {
+    console.log('geo error');
+    /* geolocation IS NOT available */
+  }
+
+  //  navigator.mediaDevices.getDisplayMedia(); CAPTURE ECRAN
